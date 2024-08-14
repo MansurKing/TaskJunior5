2cf4a51afe956e00e7e1fb4c41ca85fd68733ce0
MansurKing <102825225+MansurKing@users.noreply.github.com>
2024-08-14 12:48:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TaskJunior5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string coffee = "кофе";
+            string tea = "чай";
+            string temporaryBufferForCoffee = tea;
+
+            Console.WriteLine("Приветствую тебя дорогой пользователь, что решился на тестирование номой программе про замене жидкостей в стаканах" +
+                              "\nу нас имеется 2 жидкости и 2 стакана, а именно:\n\n - кружка с кофе\n - кружка с чаем\n");
+            Console.WriteLine($"Неправельное содерживое в чашке с кофе, а именно {tea}");
+            Console.WriteLine($"Неправельное содерживое в чашке с чаем, а именно {coffee}\n\n");
+            Console.WriteLine("Меняем содержимое чашек в правельный\n\n");
+
+            tea = coffee;
+            coffee = temporaryBufferForCoffee;
+
+            Console.WriteLine($"Правельное содерживое в чашке с кофе, а именно {tea}");
+            Console.WriteLine($"Правельное содерживое в чашке с чаем, а именно {coffee}");
+
+        }
+    }
+}
